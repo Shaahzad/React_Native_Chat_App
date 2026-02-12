bb7217b9f099b69d7197efe9976822ba8d8e6d18
Shaahzad <shehzadmemon147@gmail.com>
2026-02-12 23:59:09 +0500

    Working

--- chat-App/app/(main)/home.tsx
+++ chat-App/app/(main)/home.tsx
@@ -1,11 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useAuth } from '@/context/authContext'
+import ScreenWrapper from '../component/ScreenWrapper'
+import Typo from '../component/Typo'
+import { colors } from '@/theme'
+import Button from '../component/Button'
 
 const Home = () => {
+const {user, signOut} = useAuth()
+
+
+const HandleLogout = async() => {
+
+}
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScreenWrapper>
+      <Typo color={colors.white}>Home</Typo>
+
+
+      <Button onPress={HandleLogout}>
+        <Typo>Logout</Typo>
+      </Button>
+    </ScreenWrapper>
   )
 }
 
